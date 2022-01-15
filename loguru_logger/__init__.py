6a7e8c5aca26417d823c4993523bf20fc988fab0
doctor3030 <dimamano@gmail.com>
2022-01-15 17:45:19 -0500

    v0.0.1

--- loguru_logger/__init__.py
+++ loguru_logger/__init__.py
@@ -1,1 +1,23 @@
+from __future__ import absolute_import
+
+__title__ = 'loguru-logger'
+__author__ = 'Dmitry Amanov'
+__license__ = 'Apache License 2.0'
+__copyright__ = 'Copyright 2022 Dmitry Amanov'
+
+# Set default logging handler to avoid "No handler found" warnings.
+import logging
+try:  # Python 2.7+
+    from logging import NullHandler
+except ImportError:
+    class NullHandler(logging.Handler):
+        def emit(self, record):
+            pass
+
+logging.getLogger(__name__).addHandler(NullHandler())
+
 from logger import Logger, Sinks
+
+__all__ = [
+    'Logger', 'Sinks',
+]
